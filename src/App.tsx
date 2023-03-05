3ae87cc2743c172cbc59908470c8e7f52deeeba4
F4isalSH <f4isalcode@gmail.com>
2023-03-05 10:22:51 +0300

    prettied

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,20 @@ import { Box } from "@chakra-ui/react";
 import { Register } from "./pages/Register";
 import { EventPage } from "./pages/EventPage";
 import { BrowseEvents } from "./pages/BrowseEvents";
+import { Events } from "./pages/Event";
 
 export const App: React.FC = () => {
   return (
-    <Box>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Navigate to="/login" />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/event/:slug" element={<EventPage />} />
-          <Route path="/events" element={<BrowseEvents />} />
-          <Route path="*" element={<Navigate to="/login" />} />
-        </Routes>
-      </BrowserRouter>
-    </Box>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/event/:slug" element={<EventPage />} />
+        <Route path="/events" element={<BrowseEvents />} />
+        <Route path="/club-dashboard/event" element={<Events />} />
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
