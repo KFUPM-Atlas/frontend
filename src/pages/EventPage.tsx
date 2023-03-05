549fdbf4edd368de10c2e50bda10fc96c0384ec3
F4isalSH <f4isalcode@gmail.com>
2023-03-05 10:22:42 +0300

    style: enhancing responsiveness

--- src/pages/EventPage.tsx
+++ src/pages/EventPage.tsx
@@ -10,24 +10,28 @@ import {
   TabPanels,
   Button,
   Text,
-} from "@chakra-ui/react"
-import { Overview } from "../components/Overview"
-import { TabElement } from "../components/TabElement"
-import { COLORS } from "../core/constants"
-import { BsArrowRight } from "react-icons/bs"
-import toast from "react-hot-toast"
+} from "@chakra-ui/react";
+import { Overview } from "../components/Overview";
+import { TabElement } from "../components/TabElement";
+import { COLORS } from "../core/constants";
+import { BsArrowRight } from "react-icons/bs";
+import toast from "react-hot-toast";
 
 export const EventPage: React.FC = () => {
   return (
-    <Stack spacing={0}>
+    <Stack
+      spacing={0}
+      flexDir={{ base: "column", lg: "row-reverse" }}
+      minH="100vh"
+    >
       <Image
         src="/hackathon.jpeg"
         objectFit="fill"
         boxShadow="md"
         minH="60vh"
-        w="full"
+        w={{ base: "100%", lg: "50%" }}
       />
-      <Box minH="40vh" w="full" boxShadow="lg" p={8}>
+      <Box minH="40vh" w={{ base: "100%", lg: "50%" }} boxShadow="lg" p={8}>
         <HStack>
           <Heading size="lg" color={COLORS.PRIMARY}>
             React Hackathon
@@ -58,5 +62,5 @@ export const EventPage: React.FC = () => {
         </Button>
       </Box>
     </Stack>
-  )
-}
+  );
+};
