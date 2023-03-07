c3a4553d498594e85c5a82de4dffba99aea54ea1
F4isalSH <f4isalcode@gmail.com>
2023-03-07 12:09:46 +0300

    modification: adding avatar and divider

--- src/pages/BrowseEvents.tsx
+++ src/pages/BrowseEvents.tsx
@@ -1,4 +1,11 @@
-import { Box, Container, Heading, HStack } from "@chakra-ui/react";
+import {
+  Avatar,
+  Box,
+  Container,
+  Divider,
+  Heading,
+  HStack,
+} from "@chakra-ui/react";
 import { MobileNavbar } from "../components/MobileNavbar";
 import { EventList } from "../components/EventList";
 import { COLORS } from "../core/constants";
@@ -7,15 +14,18 @@ export const BrowseEvents: React.FC = () => {
     <>
       <Box minH="100vh">
         <Container maxW="container.lg">
-          <Heading
-            size="lg"
-            fontWeight="bold"
-            fontSize={29}
-            color={COLORS.PRIMARY}
-            py={8}
-          >
-            Overview
-          </Heading>
+          <HStack py={6} justifyContent="space-between" alignItems="center">
+            <Heading
+              size="lg"
+              fontWeight="bold"
+              fontSize={29}
+              color={COLORS.PRIMARY}
+            >
+              Overview
+            </Heading>
+            <Avatar name="Faisal Alshawan" size="sm" />
+          </HStack>
+          <Divider />
           <EventList />
         </Container>
       </Box>
