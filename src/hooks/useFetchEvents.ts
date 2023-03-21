889fd75939be5d4be671f76c8633078a1e6ce483
F4isalSH <f4isalcode@gmail.com>
2023-03-22 00:08:30 +0300

    new: created useFetchEvents

---
+++ src/hooks/useFetchEvents.ts
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
+import { db } from "../core/firebase";
+import { collection, query, onSnapshot } from "@firebase/firestore";
+
+export const useFetchEvents = () => {
+  const [events, setEvents] = useState([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string>("");
+
+  const fetchEvents = async () => {
+    const q = query(collection(db, "events"));
+    setLoading(true);
+    const unsub = onSnapshot(q, (snapshot) => {
+      let results = [];
+      snapshot.docs.forEach((doc) => {
+        results.push({ ...doc?.data(), id: doc?.id });
+      });
+      setEvents(results);
+    });
+    setLoading(false);
+    return () => unsub();
+  };
+
+  useEffect(() => {
+    try {
+      fetchEvents();
+    } catch (err) {
+      toast.error(err.message);
+      setError(err.message);
+    }
+  }, []);
+  return { events, loading, error };
+};
