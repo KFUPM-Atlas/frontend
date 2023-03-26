6e8f25117b2b8ce7bf83f5661040570f44c0cdd7
abdulelah1 <a.alharabi@outlook.com>
2023-03-26 23:41:37 +0300

    Supervisor Requests Page

---
+++ src/utils/supervisor_link_items.ts
@@ -0,0 +1,8 @@
+import {LinkItemProps} from "./link_items";
+import {FiMail, FiUsers, FiHome} from "react-icons/fi";
+
+export const supervisorLinkItems: Array<LinkItemProps> = [
+    { name: "الطلبات", icon: FiMail },
+    { name: "النوادي", icon: FiUsers },
+    { name: "صفحة القسم", icon: FiHome },
+];
