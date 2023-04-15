9c43de2276eee6b844d83806a5cf065ba18937fa
F4isalSH <f4isalcode@gmail.com>
2023-04-16 02:20:37 +0300

    new: adding desktop navbar

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,19 @@ import { Tickets } from "./pages/Tickets";
 import { Overview } from "./pages/Overview";
 import { Requests } from "./pages/Requests";
 import { ClubProfile } from "./pages/ClubProfile";
+import { DesktopNavbar } from "./components/DesktopNavbar";
 
 export const App: React.FC = () => {
+  // Define a layout component that wraps around the page components
+  const Layout = ({ children }) => {
+    return (
+      <>
+        <DesktopNavbar />
+        {children}
+      </>
+    );
+  };
+
   return (
     <BrowserRouter>
       <Routes>
@@ -20,18 +31,38 @@ export const App: React.FC = () => {
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/event/:slug" element={<EventPage />} />
-        <Route path="/events" element={<BrowseEvents />} />
         <Route path="/club-dashboard/event" element={<Events />} />
         <Route path="/club/:id" element={<ClubPage />} />
-         <Route path="/club/events" element={<Events />} />
-          <Route path="/club/overview" element={<Overview />} />
-          <Route path="/club/requests" element={<Requests />} />
-          <Route path="/club/profile" element={<ClubProfile />} />
-        <Route path="/explore" element={<Explore />} />
-        <Route path="/tickets" element={<Tickets />} />
+        <Route path="/club/events" element={<Events />} />
+        <Route path="/club/overview" element={<Overview />} />
+        <Route path="/club/requests" element={<Requests />} />
+        <Route path="/club/profile" element={<ClubProfile />} />
+        <Route
+          path="/explore"
+          element={
+            <Layout>
+              <Explore />
+            </Layout>
+          }
+        />
+        <Route
+          path="/tickets"
+          element={
+            <Layout>
+              <Tickets />
+            </Layout>
+          }
+        />
+        <Route
+          path="/events"
+          element={
+            <Layout>
+              <BrowseEvents />
+            </Layout>
+          }
+        />
         <Route path="*" element={<Navigate to="/login" />} />
-        
       </Routes>
     </BrowserRouter>
-   );
+  );
 };
