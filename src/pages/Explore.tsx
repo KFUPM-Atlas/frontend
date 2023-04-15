765cbff24e5e394acaa02ca62af0fb6d6ce0538b
F4isalSH <f4isalcode@gmail.com>
2023-04-16 02:21:09 +0300

    new: adding desktop navbar

--- src/pages/Explore.tsx
+++ src/pages/Explore.tsx
@@ -2,6 +2,7 @@ import { Box, Container, Heading, HStack } from "@chakra-ui/react";
 import { MobileNavbar } from "../components/MobileNavbar";
 import { ExploreEvents } from "../components/ExploreEvents";
 import { COLORS } from "../core/constants";
+import { DesktopNavbar } from "../components/DesktopNavbar";
 export const Explore: React.FC = () => {
   return (
     <>
