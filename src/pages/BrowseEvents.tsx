76b396ce69c90092b8be661142b709997d1dafb3
F4isalSH <f4isalcode@gmail.com>
2023-04-16 02:21:25 +0300

    new: adding menu

--- src/pages/BrowseEvents.tsx
+++ src/pages/BrowseEvents.tsx
@@ -1,17 +1,26 @@
 import {
-  Avatar,
+  Text,
   Box,
   Container,
   Divider,
   Heading,
   HStack,
   Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
 } from "@chakra-ui/react";
 import { MobileNavbar } from "../components/MobileNavbar";
 import { EventList } from "../components/EventList";
 import { COLORS } from "../core/constants";
 import { useNavigate } from "react-router";
 import { useAuthContext } from "../hooks/useAuthContext";
+import { DesktopNavbar } from "../components/DesktopNavbar";
+import { EventRow } from "../components/events/EventRow";
+import { ChevronDownIcon } from "@chakra-ui/icons";
+
 export const BrowseEvents: React.FC = () => {
   const { user } = useAuthContext();
   const navigate = useNavigate();
@@ -20,14 +29,6 @@ export const BrowseEvents: React.FC = () => {
       <Box minH="100vh">
         <Container maxW="container.lg">
           <HStack py={6} justifyContent="space-between" alignItems="center">
-            <Heading
-              size="lg"
-              fontWeight="bold"
-              fontSize={29}
-              color={COLORS.PRIMARY}
-            >
-              Overview
-            </Heading>
             {!user && (
               <Button
                 bg={"white"}
@@ -41,8 +42,32 @@ export const BrowseEvents: React.FC = () => {
               </Button>
             )}
           </HStack>
-          <Divider />
-          <EventList />
+          {/* <Divider />
+          <EventList /> */}
+          <HStack justifyContent="space-between" pb={4}>
+            <HStack>
+              <Text fontWeight="bold">My Events</Text>
+            </HStack>
+            <Menu>
+              <MenuButton
+                px={4}
+                py={2}
+                color="white"
+                transition="all 0.2s"
+                borderRadius="md"
+                borderWidth="1px"
+                bgGradient="linear(to-l, gray.600, gray.900)"
+              >
+                Most Recent <ChevronDownIcon color="green.500" />
+              </MenuButton>
+              <MenuList>
+                <MenuItem>Most Recent</MenuItem>
+                <MenuItem>Popular</MenuItem>
+                <MenuItem>Hot</MenuItem>
+              </MenuList>
+            </Menu>
+          </HStack>
+          <EventRow />
         </Container>
       </Box>
       <MobileNavbar />
