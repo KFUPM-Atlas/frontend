ea1d83811c45f5a2a08424e317b0573339422970
F4isalSH <f4isalcode@gmail.com>
2023-04-16 04:38:18 +0300

    new: new style

--- src/pages/BrowseEvents.tsx
+++ src/pages/BrowseEvents.tsx
@@ -1,33 +1,28 @@
-import {
-  Avatar,
-  Box,
-  Container,
-  Divider,
-  Heading,
-  HStack,
-  Button,
-} from "@chakra-ui/react";
-import { MobileNavbar } from "../components/MobileNavbar";
+import { Avatar, Box, Container, Text, HStack, Button } from "@chakra-ui/react";
 import { EventList } from "../components/EventList";
 import { COLORS } from "../core/constants";
 import { useNavigate } from "react-router";
 import { useAuthContext } from "../hooks/useAuthContext";
+import { SearchBox } from "../components/SearchBox";
+import { CategoryPick } from "../components/CategoryPick";
+import { MobileNavbar } from "../components/MobileNavbar";
+import { useLogout } from "../hooks/useLogout";
 export const BrowseEvents: React.FC = () => {
   const { user } = useAuthContext();
   const navigate = useNavigate();
+  const { logout } = useLogout();
+
   return (
     <>
-      <Box minH="100vh">
+      <Box minH="100vh" bgColor="#FDFDFD">
         <Container maxW="container.lg">
-          <HStack py={6} justifyContent="space-between" alignItems="center">
-            <Heading
-              size="lg"
-              fontWeight="bold"
-              fontSize={29}
-              color={COLORS.PRIMARY}
-            >
-              Overview
-            </Heading>
+          <HStack
+            borderBottomLeftRadius={20}
+            borderBottomRightRadius={20}
+            py={8}
+            px={2}
+            justifyContent="space-between"
+          >
             {!user && (
               <Button
                 bg={"white"}
@@ -40,8 +35,33 @@ export const BrowseEvents: React.FC = () => {
                 Login
               </Button>
             )}
+            {user && (
+              <Button
+                bg={"white"}
+                color={COLORS.PRIMARY}
+                boxShadow={"lg"}
+                as={"a"}
+                onClick={() => logout()}
+                fontSize="sm"
+              >
+                Logout
+              </Button>
+            )}
+            <Avatar name="Christian Nwamba" src="https://bit.ly/code-beast" />
           </HStack>
-          <Divider />
+          <SearchBox />
+          <CategoryPick
+            categories={["All", "Tech", "Business", "Entertainment", "Other"]}
+          />
+          <HStack justifyContent="space-between" pt={4}>
+            <Text fontWeight="medium">Events for you</Text>
+            <Text>See more</Text>
+          </HStack>
+
+          <HStack justifyContent="space-between" pt={4}>
+            <Text fontWeight="medium">Popular Events</Text>
+            <Text>See more</Text>
+          </HStack>
           <EventList />
         </Container>
       </Box>
