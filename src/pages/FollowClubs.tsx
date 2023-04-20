87ff56cff717eeab2b992e60c9e33af3745ee50f
F4isalSH <f4isalcode@gmail.com>
2023-04-20 18:44:33 +0300

    new: creating follow clubs page

---
+++ src/pages/FollowClubs.tsx
@@ -0,0 +1,11 @@
+import { Box, Center, HStack, Image, Container, Stack } from "@chakra-ui/react";
+import { Form } from "../components/Form";
+import { COLORS } from "../core/constants";
+import { useMediaQuery } from "@chakra-ui/react";
+export const FollowClubs: React.FC = () => {
+  return (
+    <Box>
+      <Box></Box>
+    </Box>
+  );
+};
