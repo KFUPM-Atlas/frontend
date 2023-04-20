fc75203a618f755984d4adbeff37053abf803baa
F4isalSH <f4isalcode@gmail.com>
2023-04-20 05:17:05 +0300

    new: adding icon

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -7,23 +7,19 @@ import {
   InputRightElement,
 } from "@chakra-ui/react";
 import { SearchIcon } from "@chakra-ui/icons";
-import { COLORS } from "../core/constants";
 interface SearchBoxProps {}
 
 export const SearchBox: React.FC<SearchBoxProps> = ({}) => {
   return (
     <Box>
       <Stack spacing={4}>
-        <Input
-          size="lg"
-          placeholder="Search Atlas..."
-          borderRadius={"full"}
-          boxShadow="sm"
-          bgColor="white"
-          fontWeight="regular"
-          py={8}
-          pl={10}
-        />
+        <InputGroup>
+          <InputLeftElement
+            pointerEvents="none"
+            children={<SearchIcon color="gray.300" />}
+          />
+          <Input placeholder="Hackathon..." borderRadius={10} />
+        </InputGroup>
       </Stack>
     </Box>
   );
