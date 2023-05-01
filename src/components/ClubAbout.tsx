01780897249ede0669a92e2d306c1ecfdba1513d
F4isalSH <f4isalcode@gmail.com>
2023-05-02 00:40:05 +0300

    chore: removing unused section

--- src/components/ClubAbout.tsx
+++ src/components/ClubAbout.tsx
@@ -28,59 +28,6 @@ export const ClubAbout: React.FC<ClubAboutProps> = ({}) => {
               </Heading>
               <Text>{club?.description}</Text>
             </Box>
-            <Box>
-              <Heading fontWeight="bold" fontSize={18} color={COLORS.PRIMARY}>
-                Objective
-              </Heading>
-              <HStack>
-                {club?.objectives?.map((objective, index) => (
-                  <Text key={index}>{objective}</Text>
-                ))}
-              </HStack>
-            </Box>
-            <Box>
-              <Heading fontWeight="bold" fontSize={18} color={COLORS.PRIMARY}>
-                Social Media
-              </Heading>
-              <SimpleGrid columns={2} spacing={4} pt={4}>
-                <GridItem colSpan={{ base: 1, lg: 1 }}>
-                  <HStack>
-                    <Image
-                      w={10}
-                      src="https://cdn.cdnlogo.com/logos/t/96/twitter-icon.svg"
-                    />
-                    <Text fontSize={13}>@GDSCKFUPM</Text>
-                  </HStack>
-                </GridItem>
-                <GridItem colSpan={{ base: 1, lg: 1 }}>
-                  <HStack>
-                    <Image
-                      w={10}
-                      src="https://cdn.cdnlogo.com/logos/i/92/instagram.svg"
-                    />
-                    <Text fontSize={13}>@GDSCKFUPM</Text>
-                  </HStack>
-                </GridItem>
-                <GridItem colSpan={{ base: 1, lg: 1 }}>
-                  <HStack>
-                    <Image
-                      w={10}
-                      src="https://cdn.cdnlogo.com/logos/l/78/linkedin-icon.svg"
-                    />
-                    <Text fontSize={13}>@GDSCKFUPM</Text>
-                  </HStack>
-                </GridItem>
-                <GridItem colSpan={{ base: 1, lg: 1 }}>
-                  <HStack>
-                    <Image
-                      w={10}
-                      src="https://cdn.cdnlogo.com/logos/f/83/facebook.svg"
-                    />
-                    <Text fontSize={13}>@GDSCKFUPM</Text>
-                  </HStack>
-                </GridItem>
-              </SimpleGrid>
-            </Box>
           </Stack>
         </Box>
       )}
