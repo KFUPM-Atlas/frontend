e5510ffa71f0c6e27757de7e7634197f6a48a14c
F4isalSH <f4isalcode@gmail.com>
2023-05-02 00:38:52 +0300

    new: adding clubs

--- src/components/NotSupported.tsx
+++ src/components/NotSupported.tsx
@@ -1,16 +1,55 @@
-import { Box, Center, Image, Stack, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Center,
+  Image,
+  Stack,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Button,
+  HStack,
+  Heading,
+} from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+
 import { COLORS } from "../core/constants";
+import { useAuthContext } from "../hooks/useAuthContext";
+import { useFetchUserClubs } from "../hooks/useFetchUserClubs";
 interface NotSupportedProps {}
 
 export const NotSupported: React.FC<NotSupportedProps> = ({}) => {
+  const { user } = useAuthContext();
+  const { clubs } = useFetchUserClubs(user?.uid);
+  const navigate = useNavigate();
+
   return (
     <Box minH="100vh" w="full" bgColor={COLORS.PRIMARY} boxShadow="lg">
-      <Center minH="100vh">
+      {clubs.length >= 1 && (
+        <HStack justifyContent="space-between" p={10}>
+          <Heading fontSize="2xl" fontWeight="bold" color="white">
+            For event exploration, kindly use mobile view.
+          </Heading>
+
+          <Menu>
+            <MenuButton as={Button}>Clubs</MenuButton>
+            <MenuList>
+              {clubs.map((club) => (
+                <MenuItem
+                  onClick={() => navigate(`/club/${club.clubId}/profile`)}
+                >
+                  {club.name}
+                </MenuItem>
+              ))}
+            </MenuList>
+          </Menu>
+        </HStack>
+      )}
+      <Center>
         <Stack color="white">
-          <Text>
-            Desktop will be supported soon! For now kindly switch to mobile.
-          </Text>
-          <Image src="./illustration.svg" w={400} />
+          <Center>
+            <Image src="./illustration.svg" w={400} />
+          </Center>
         </Stack>
       </Center>
     </Box>
