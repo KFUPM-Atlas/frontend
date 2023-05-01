5b964cbdb772034460d218a49c1f06018e9bbf62
F4isalSH <f4isalcode@gmail.com>
2023-05-02 00:38:45 +0300

    new: using filter store

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -7,9 +7,18 @@ import {
   InputRightElement,
 } from "@chakra-ui/react";
 import { SearchIcon } from "@chakra-ui/icons";
+import { useFiltersStore } from "../stores/useFiltersStore";
+import { useEffect, useState } from "react";
 interface SearchBoxProps {}
 
 export const SearchBox: React.FC<SearchBoxProps> = ({}) => {
+  const { setSearch, search } = useFiltersStore();
+  const [userSearch, setUserSearch] = useState("");
+
+  useEffect(() => {
+    setSearch(userSearch);
+  }, [userSearch]);
+
   return (
     <Box>
       <Stack spacing={4}>
@@ -23,6 +32,8 @@ export const SearchBox: React.FC<SearchBoxProps> = ({}) => {
             borderRadius={20}
             border="1px"
             borderColor="gray.200"
+            value={userSearch}
+            onChange={(e) => setUserSearch(e.target.value)}
           />
         </InputGroup>
       </Stack>
