849d0141677d1c02022e5ca6282fb05369661cf5
F4isalSH <f4isalcode@gmail.com>
2023-05-02 00:37:58 +0300

    new: created useFetchUserClubs

---
+++ src/hooks/useFetchUserClubs.ts
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
+import { db } from "../core/firebase";
+import { collection, query, onSnapshot, where } from "@firebase/firestore";
+
+export const useFetchUserClubs = (uid: string) => {
+  const [clubs, setClubs] = useState([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string>("");
+
+  const fetchUserClubs = async () => {
+    const q = query(
+      collection(db, "clubs"),
+      where("isDeleted", "==", false),
+      where("presidentUid", "==", uid)
+    );
+    setLoading(true);
+    const unsub = onSnapshot(q, (snapshot) => {
+      let results = [];
+      snapshot.docs.forEach((doc) => {
+        results.push({ ...doc?.data(), id: doc?.id });
+      });
+      setClubs(results);
+    });
+    setLoading(false);
+    return () => unsub();
+  };
+
+  useEffect(() => {
+    try {
+      fetchUserClubs();
+    } catch (err) {
+      toast.error(err.message);
+      setError(err.message);
+    }
+  }, []);
+  return { clubs, loading, error };
+};
